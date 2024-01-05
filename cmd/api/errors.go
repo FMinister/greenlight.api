53326bff6130365327146f9957de72a78ae04bea
FMinister <johannes.leyrer+git@googlemail.com>
2024-01-05 23:35:42 +0100

    update errors for failed validation

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -38,6 +38,10 @@ func (app *application) methodNotAllowedResponse(w http.ResponseWriter, r *http.
 	app.errorResponse(w, r, http.StatusMethodNotAllowed, message)
 }
 
+func (app *application) failedValidationResponse(w http.ResponseWriter, r *http.Request, errors map[string]string) {
+	app.errorResponse(w, r, http.StatusUnprocessableEntity, errors)
+}
+
 func (app *application) serverErrorResponse(w http.ResponseWriter, r *http.Request, err error) {
 	app.logError(r, err)
 
