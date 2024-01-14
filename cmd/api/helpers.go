312f8490ef0cc65ab6134f3863cdc1c5b2f9d3bc
FMinister <johannes.leyrer+git@googlemail.com>
2024-01-14 15:45:22 +0100

    outsource background recovery method

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -132,3 +132,16 @@ func (app *application) readInt(qs url.Values, key string, defaultValue int, v *
 
 	return intValue
 }
+
+// recovery method to use in go routines
+func (app *application) background(fn func()) {
+	go func() {
+		defer func() {
+			if err := recover(); err != nil {
+				app.logger.Error(fmt.Sprintf("%v", err))
+			}
+		}()
+
+		fn()
+	}()
+}
