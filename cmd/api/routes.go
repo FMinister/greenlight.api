7599075b33566b217e0c3b064ad340217fd50355
FMinister <johannes.leyrer+git@googlemail.com>
2024-02-05 15:40:55 +0100

    add metrics middleware for request duration

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -26,8 +26,8 @@ func (app *application) routes() http.Handler {
 
 	router.HandlerFunc(http.MethodPost, "/v1/tokens/authentication", app.createAuthenticationTokenHandler)
 
-	// router.Handler(http.MethodGet, "/v1/metrics", expvar.Handler())
-	router.HandlerFunc(http.MethodGet, "/v1/metrics", app.requirePermission("metrics:view", expvar.Handler().ServeHTTP))
+	router.Handler(http.MethodGet, "/v1/metrics", expvar.Handler())
+	// router.HandlerFunc(http.MethodGet, "/v1/metrics", app.requirePermission("metrics:view", expvar.Handler().ServeHTTP))
 
-	return app.recoverPanic(app.enableCORS(app.rateLimit(app.authenticate(router))))
+	return app.metrics(app.recoverPanic(app.enableCORS(app.rateLimit(app.authenticate(router)))))
 }
